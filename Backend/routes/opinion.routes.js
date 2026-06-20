e5e05e39521eccea55804d23e6ccab255810d454
Gael <gaelenriquemedinamartinez@gmail.com>
2026-06-19 21:18:52 -0600

    n

--- Backend/routes/opinion.routes.js
+++ Backend/routes/opinion.routes.js
@@ -1,10 +1,18 @@
+const express = require("express");
+const router = express.Router();
+
+const opinionController = require("../controllers/opinion.controller");
+const verificarToken = require("../middleware/auth.middleware");
+
 router.post(
     "/opinion",
     verificarToken,
-    controller.crearOpinion
+    opinionController.crearOpinion
 );
 
 router.get(
     "/reputacion/:id",
-    controller.obtenerReputacion
+    opinionController.obtenerReputacion
 );
+
+module.exports = router;
